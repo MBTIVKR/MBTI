3e2ab0f96fd9e32f59a4aea9e8fa4f2d19df6ba6
Avdushin <avdushinbeaver1@gmail.com>
2024-01-27 03:50:46 +0300

    Added more ansewrs options

--- src/Components/mbti/mbti.jsx
+++ src/Components/mbti/mbti.jsx
@@ -7,10 +7,10 @@ const MBTITest = () => {
   const [result, setResult] = useState(null);
   const [showResult, setShowResult] = useState(false);
 
-  const handleAnswer = (questionId, answer) => {
+  const handleAnswer = (questionId, level) => {
     setAnswers((prevAnswers) => ({
       ...prevAnswers,
-      [questionId]: answer,
+      [questionId]: level,
     }));
 
     if (currentQuestion < questions.length - 1) {
@@ -25,91 +25,98 @@ const MBTITest = () => {
     console.log('Current Question:', currentQuestion);
     console.log('Show Result:', showResult);
 
-    // Количество вопросов
+    // Получаем количество вопросов
     const totalQuestions = questions.length;
 
     const checkResult = () => {
-      // Инициализация счетчиков для каждой характеристики, включая "Не знаю"
+      // Инициализация счетчиков для каждой характеристики
       let extraversionCount = 0;
       let sensingCount = 0;
       let thinkingCount = 0;
       let judgingCount = 0;
-      let doNotKnowCount = 0;
 
       // Перебираем все ответы и увеличиваем соответствующие счетчики
-      Object.values(answers).forEach((answer) => {
-        switch (answer) {
-          case 'Согласен':
+      Object.entries(answers).forEach(([_, level]) => {
+        switch (level) {
+          case 3:
             extraversionCount++;
             sensingCount++;
             thinkingCount++;
             judgingCount++;
             break;
-          case 'Не согласен':
+          case 2:
+            extraversionCount += 0.5;
+            sensingCount += 0.5;
+            thinkingCount += 0.5;
+            judgingCount += 0.5;
             break;
-          case 'Не знаю':
-            doNotKnowCount += 0;
+          case 1:
+            break;
+          case 0:
+            break;
+          case -1:
+            extraversionCount -= 0.5;
+            sensingCount -= 0.5;
+            thinkingCount -= 0.5;
+            judgingCount -= 0.5;
+            break;
+          case -2:
+            extraversionCount--;
+            sensingCount--;
+            thinkingCount--;
+            judgingCount--;
+            break;
+          case -3:
+            extraversionCount--;
+            sensingCount--;
+            thinkingCount--;
+            judgingCount--;
             break;
           default:
             break;
         }
       });
 
-      // Логика для определения каждой из четырех бинарных характеристик, включая "Не знаю"
-      const extraversion =
-        extraversionCount + doNotKnowCount >= totalQuestions / 2 ? 'E' : 'I';
-      const sensing =
-        sensingCount + doNotKnowCount >= totalQuestions / 2 ? 'S' : 'N';
-      const thinking =
-        thinkingCount + doNotKnowCount >= totalQuestions / 2 ? 'T' : 'F';
-      const judging =
-        judgingCount + doNotKnowCount >= totalQuestions / 2 ? 'J' : 'P';
+      // Логика для определения каждой из четырех бинарных характеристик
+      const extraversion = extraversionCount >= totalQuestions / 2 ? 'E' : 'I';
+      const sensing = sensingCount >= totalQuestions / 2 ? 'S' : 'N';
+      const thinking = thinkingCount >= totalQuestions / 2 ? 'T' : 'F';
+      const judging = judgingCount >= totalQuestions / 2 ? 'J' : 'P';
 
       // Собираем все четыре характеристики вместе, чтобы получить полный тип личности
       const personalityType = extraversion + sensing + thinking + judging;
 
-      {localStorage.setItem('type', personalityType)}
       // Устанавливаем результат в состояние
       setResult(`Ваш тип личности: ${personalityType}`);
     };
 
     if (showResult && Object.keys(answers).length === totalQuestions) {
       checkResult();
-
-      localStorage.setItem('answers', JSON.stringify(answers));
     }
-  }, [answers, currentQuestion, showResult, questions]);
+  }, [answers, currentQuestion, showResult]);
 
   return (
     <div>
       <h1>Тест MBTI</h1>
       {currentQuestion < questions.length && (
         <div>
-          <p>{questions[currentQuestion].text}</p>
-          {questions[currentQuestion].options.map((option, index) => (
-            <button
-              key={index}
-              onClick={() =>
-                handleAnswer(questions[currentQuestion].id, option)
-              }
-            >
-              {option}
-            </button>
-          ))}
+        <p className='question'>{questions[currentQuestion].text}</p>
+        <div>
+            {questions[currentQuestion].options.map((option, index) => (
+                <button key={index} onClick={() => handleAnswer(questions[currentQuestion].id, index - 3)}>
+                    {option}
+                </button>
+            ))}
         </div>
+    </div>
+    
       )}
       {showResult && (
         <div>
           <h2>Результаты теста</h2>
-          {result ? (
-          <p>{result}</p>
-          )
-           : <p>Вычисление результата...</p>}
+          {result ? <p>{result}</p> : <p>Вычисление результата...</p>}
         </div>
-
-
       )}
-      {/* {localStorage.setItem('type', result)} */}      
     </div>
   );
 };
